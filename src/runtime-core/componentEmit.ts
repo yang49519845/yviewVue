0a83dec402c9e0c2d117eec127ff43c6e89c5998
杨鑫 <1342039458@qq.com>
2022-04-07 16:19:35 +0800

    emit

--- src/runtime-core/componentEmit.ts
+++ src/runtime-core/componentEmit.ts
@@ -1,17 +1,9 @@
+import { camelize, toHandlerKey } from "../shared/index";
+
 export function emit(instance, event, ...args) {
   const { props } = instance;
 
-  const camelize = (str: string) => {
-    return str.replace(/-(\w)/g, (_, c: string) => { return c ? c.toUpperCase() : '' })
-  }
 
-  const capitalize = (str: string) => {
-    return str.charAt(0).toUpperCase() + str.slice(1);
-  }
-
-  const toHandlerKey = (str: string) => {
-    return str ? 'on' + capitalize(str) : ''
-  }
 
   const handlerName = toHandlerKey(camelize(event))
   const handler = props[handlerName];
--- src/shared/index.ts
+++ src/shared/index.ts
@@ -8,3 +8,15 @@ export const hasChanged = (newValue, val) => !Object.is(newValue, val);
 
 
 export const hasOwn = (val, key) => Object.prototype.hasOwnProperty.call(val, key)
+
+export const camelize = (str: string) => {
+  return str.replace(/-(\w)/g, (_, c: string) => { return c ? c.toUpperCase() : '' })
+}
+
+export const capitalize = (str: string) => {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+export const toHandlerKey = (str: string) => {
+  return str ? 'on' + capitalize(str) : ''
+}
